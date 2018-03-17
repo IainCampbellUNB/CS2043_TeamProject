a9d38c56c736dce02ee8684f482e31c083001ae4
GabbieDupuis <36309033+GabbieDupuis@users.noreply.github.com>
2018-03-17 14:28:11 -0300

    Add files via upload

---
+++ Schedule.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Schedule
+{
+  private ArrayList<String> schedule;
+
+  public Schedule(ArrayList<String> scheduleIn)
+  {
+    schedule = scheduleIn;
+  }
+
+  public getSchedule()
+  {
+    return schedule;
+  }
+}
