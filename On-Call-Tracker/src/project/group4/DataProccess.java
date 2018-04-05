0cd6ca36016d99063a555ab51499a17038453211
Craig Beaman <Craig.Beaman@unb.ca>
2018-04-05 19:13:39 -0300

    Update DataProccess.java

--- On-Call-Tracker/src/project/group4/DataProccess.java
+++ On-Call-Tracker/src/project/group4/DataProccess.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public class DataProccess 
 {
 	private ArrayList<OnCallTeacher> teacherList;
-	ArrayList<Teacher> supplyList;
-	AbsenceWorkbookReader reader;
-	TallyWorkbookReader tallyreader;
+	private ArrayList<Teacher> supplyList;
+	private AbsenceWorkbookReader reader;
+	private TallyWorkbookReader tallyreader;
 	
 	public DataProccess(AbsenceWorkbookReader reader, TallyWorkbookReader tallyreader)
 	{
@@ -17,7 +17,6 @@ public class DataProccess
 		this.supplyList = new ArrayList<Teacher>();
 		this.reader = reader;
 		this.tallyreader = tallyreader;
-		
 	}
 	
 	public ArrayList<OnCallTeacher> createTeacherTermSchedule() throws IOException, ParseException
@@ -25,30 +24,30 @@ public class DataProccess
 		ArrayList<ArrayList<String>> termScheduleData = new ArrayList<ArrayList<String>>();
 		termScheduleData = reader.readTermSchedule();
 		
-		for(int row = 0; row < termScheduleData.size(); row++){
-			
+		for(int row = 0; row < termScheduleData.size(); row++)
+		{
 			String id = termScheduleData.get(row).get(0);
-		    String name = termScheduleData.get(row).get(1);
-		    String p1 = termScheduleData.get(row).get(2);
-		    String p2 = termScheduleData.get(row).get(3);
-		    String p3a = termScheduleData.get(row).get(4);
-		    String p3b = termScheduleData.get(row).get(5);
-		    String p4 = termScheduleData.get(row).get(6);
-		    Schedule sched = new Schedule(p1,p2,p3a,p3b,p4); 
-		    OnCallTeacher obj1 = new OnCallTeacher(id,name,sched);
-		    teacherList.add(obj1);
+		   	String name = termScheduleData.get(row).get(1);
+		    	String p1 = termScheduleData.get(row).get(2);
+		    	String p2 = termScheduleData.get(row).get(3);
+		    	String p3a = termScheduleData.get(row).get(4);
+		   	String p3b = termScheduleData.get(row).get(5);
+		    	String p4 = termScheduleData.get(row).get(6);
+		    	Schedule sched = new Schedule(p1,p2,p3a,p3b,p4); 
+		    	OnCallTeacher obj1 = new OnCallTeacher(id,name,sched);
+		    	teacherList.add(obj1);
 		}
+		
 		assignAbsences();
 		assignTallys();
-		
 		return teacherList;
-	
 	}
 	
 	public ArrayList<Teacher> createSupplyList() throws IOException, ParseException
 	{
 		ArrayList<ArrayList<String>> supplyListData = new ArrayList<ArrayList<String>>();
 		supplyListData = reader.readSupplyList();
+		
 		for(int row = 0; row < supplyListData.size(); row++)
 		{
 			String id = supplyListData.get(row).get(0);
@@ -56,7 +55,6 @@ public class DataProccess
 			SupplyTeacher obj1 = new SupplyTeacher(id,name);
 			supplyList.add(obj1);
 		}
-	
 		
 		return supplyList;
 	}
@@ -65,6 +63,7 @@ public class DataProccess
 	{
 		ArrayList<ArrayList<String>> absenceTrackerData = new ArrayList<ArrayList<String>>();
 		absenceTrackerData = reader.readAbsenceTracker();
+		
 		for(int row = 0; row < absenceTrackerData.size(); row++)
 		{
 			String name = absenceTrackerData.get(row).get(0);
@@ -74,7 +73,8 @@ public class DataProccess
 			String p3b = absenceTrackerData.get(row).get(4);
 			String p4 = absenceTrackerData.get(row).get(5);
 			
-			AbsenceTracker tracker = new AbsenceTracker(p1,p2,p3a,p3b,p4);		
+			AbsenceTracker tracker = new AbsenceTracker(p1,p2,p3a,p3b,p4);
+			
 			if(tracker.checkAbsencePresent())
 			{
 				for(int i = 0; i< teacherList.size(); i++)
@@ -86,36 +86,36 @@ public class DataProccess
 						teacherList.get(i).submitAbsenceSchedule(tracker);
 					}
 				}
-				
-			 }
-		
+			}
 		}	
-
 	}
 	
-	private void assignTallys() throws IOException, ParseException{
+	private void assignTallys() throws IOException, ParseException
+	{
 		ArrayList<ArrayList<String>> tallyCountData = new ArrayList<ArrayList<String>>();
 		tallyCountData = tallyreader.readTallyCount();
 		
-		for(int row = 0; row < tallyCountData.size(); row++){
+		for(int row = 0; row < tallyCountData.size(); row++)
+		{
 			String name = tallyCountData.get(row).get(0);
 			String weeklyTally = tallyCountData.get(row).get(1);
 			String monthTally = tallyCountData.get(row).get(2);
 			String TermTally = tallyCountData.get(row).get(3);
 			
 			int i = 0;
-			boolean match = false;
-			while( i< teacherList.size() && !match)
+
+			while( i< teacherList.size())
 			{
 				if(teacherList.get(i).getName().equals(name))
 				{
 					teacherList.get(i).setWeeklyTally(weeklyTally);
 					teacherList.get(i).setMonthlyTally(monthTally);
 					teacherList.get(i).setTermTally(TermTally);
-					match = true;
+					break;
 				}
 				i++;
 			}
 		}
 	}
+	
 }
